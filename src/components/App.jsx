a155e6e0762500d09440f84192c9802a9257db52
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2025-05-09 00:24:48 +0300

    update huks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './Header/Header';
 import { MainPage } from './MainPage/MainPage';
 import { Container } from './global.styled';
 import { getProductCategory } from 'API/api';
 import { CategoryList } from './CategoryList/CategoryList';
+import { useEffect } from 'react';
 
-export default class App extends Component {
-  state = {
-    isLoading: false,
-    error: '',
-    products: null,
-  };
+export const App = () => {
+  // const [ isLoading, setIsLoading ] = useState(false);
+  const [error, setError] = useState(null);
+  const [products, setProducts] = useState(null);
 
-  componentDidMount() {
-    this.getAllCategory();
-  }
+  useEffect(() => {
+    const getAllCategory = async () => {
+      try {
+        const data = await getProductCategory();
+        console.log(data);
+        const firstFive = data.slice(0, 5);
+        console.log('FISRST FIVE:', firstFive);
+        setProducts(firstFive);
+      } catch (error) {
+        console.log(error);
+        setError(error.response.data);
+      }
+    };
+    getAllCategory();
+  }, []);
 
-  getAllCategory = async () => {
-    try {
-      const data = await getProductCategory();
-      const firstFive = data.slice(0, 5);
-      console.log('FISRST FIVE:', firstFive);
-      this.setState({ products: firstFive, error: '' });
-    } catch (error) {
-      console.log(error);
-      this.setState({ error: error.response.data });
-    }
-  };
-  render() {
-    const { products, error } = this.state;
-    return (
-      <div>
-        <Header />
-        <Container>
-          <CategoryList products={products} error={error} />
-          <MainPage products={products} />
-        </Container>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header />
+      <Container>
+        <CategoryList products={products} error={error} />
+        <MainPage products={products} />
+      </Container>
+    </div>
+  );
+};
+
+// export default class App extends Component {
+//   state = {
+//     isLoading: false,
+//     error: '',
+//     products: null,
+//   };
+
+//   componentDidMount() {
+//     this.getAllCategory();
+//   }
+
+//   getAllCategory = async () => {
+//     try {
+//       const data = await getProductCategory();
+//       console.log(data);
+//       const firstFive = data.slice(0, 5);
+//       console.log('FISRST FIVE:', firstFive);
+//       this.setState({ products: firstFive, error: '' });
+//     } catch (error) {
+//       console.log(error);
+//       this.setState({ error: error.response.data });
+//     }
+//   };
+//   render() {
+//     const { products, error } = this.state;
+//     return (
+//       <div>
+//         <Header />
+//         <Container>
+//           <CategoryList products={products} error={error} />
+//           <MainPage products={products} />
+//         </Container>
+//       </div>
+//     );
+//   }
+// }
